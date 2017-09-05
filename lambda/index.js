4d0413c8b47c4cf45bbcb749d41e9e47c914e3e3
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-09-05 09:04:59 +0100

    lambda

--- lambda/index.js
+++ lambda/index.js
@@ -1,34 +1,68 @@
+const doc = require('dynamodb-doc');
+const dynamo = new doc.DynamoDB();
+const Q = require('kew');
+
 function formUrls(paths) {
-	return paths.map(x => "http://api.nextgen.guardianapps.co.uk" + x + "/lite.json");
+    return paths.map(x => "http://api.nextgen.guardianapps.co.uk" + x + "/lite.json");
 }
 
-var fetch = require("node-fetch");
-var frontsPaths = require("./paths");
-var paths = frontsPaths.pathsList;
-var urls = formUrls(paths);
-var i = 0;
-var promises = urls.map(l => fetch(l));
-var fetchResponses = Promise.all(promises).then(function(responses) {
-        return responses.map(r => r.json());
-    });
-var jsonResponses = fetchResponses.then(function(responses) {
-    Promise.all(responses).then(function(json){
-    	var pathWithLinks = {};
-    	var objects = json.map((element,index) => {
-    		pathWithLinks[paths[index]] = [];
-    		element.collections.map((collection, containerIndex) => {
-    			collection.content.map((content, contentIndex) => {
-    				//add 
-    			 	pathWithLinks[paths[index]].push({
-    			 		"link" : content.id,
-    			 		"containerIndex" : containerIndex,
-    			 		"containerName" : collection.displayName,
-    			 		"contentIndex" : contentIndex
-    			 	});
-    			});
-    		});
-    	}); 
+exports.handler = function (event, context, callback) {
 
-    	console.log(pathWithLinks["/uk"])	;
-    });
-});    
+    function putItem(json) {
+        const defer = Q.defer();
+        var date = new Date();
+        const query = event.queryStringParameters;
+        const params = {
+          TableName: 'bechdel-fronts',
+          Item: json
+        };
+ 
+
+        dynamo.putItem(
+            params, 
+            defer.makeNodeResolver()
+        );
+        return defer.promise;
+    }
+   
+    var fetch = require("node-fetch");
+    var frontsPaths = require("./paths");
+    var paths = frontsPaths.pathsList;
+    var urls = formUrls(paths);
+    var promises = urls.map(l => fetch(l));
+    var fetchResponses = Promise.all(promises).then(function(responses) {
+            return responses.map(r => r.json());
+        });
+    var jsonResponses = fetchResponses.then(function(responses) {
+        Promise.all(responses).then(function(json){
+            var objects = json.map((element,index) => {
+                var item = {};
+                var date = new Date();
+                item["time"] = date.toString();
+                item["front"] = paths[index];
+                item["links"] = [];
+                if(element.collections){
+                    element.collections.map((collection, containerIndex) => {
+                        if(collection.content){
+                            collection.content.map((content, contentIndex) => {
+                                //add         
+                                var linkData = {
+                                    link : content.id,
+                                    containerIndex : containerIndex,
+                                    containerName : collection.displayName,
+                                    contentIndex : contentIndex
+                                };
+                                item["links"].push(linkData);                                                        
+                            });
+                        }
+                    });
+    
+                }
+                putItem(item) 
+                .then(data => response(data, 200))
+                .fail(err => response(err, 500));
+            }); 
+                       
+        });
+    });   
+} 
