7d487a7924714f1c095430e5889d5c6703a1e5fe
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-06-03 19:03:07 +0100

    pg works

--- lambdas/gu-bechdel-fronts-analyser/index.js
+++ lambdas/gu-bechdel-fronts-analyser/index.js
@@ -1,23 +1,28 @@
-var { Pool, Client } = require('pg');
-var pool = new Pool({
-  user: 'bechdelmaster',
-  host: 'bechdel-fronts.cii9twl865uw.eu-west-1.rds.amazonaws.com',
-  database: 'fronts',
-  password: process.env.PGPASSWORD,
-  port: 5432,
-})
+const { Pool, Client } = require('pg');
+const uuidv1 = require('uuid/v1');
+const format = require('pg-format');
 
 
 function x() {
-    pool.query('SELECT count(*) as abc from links', (err, res) => {
-    console.log(err, res);
-    pool.end()
-})
+    const pool = new Pool({
+      user: 'bechdelmaster',
+      host: 'bechdel-fronts.cii9twl865uw.eu-west-1.rds.amazonaws.com',
+      database: 'fronts',
+      password: process.env.PGPASSWORD,
+      port: 5432,
+    })
 
+    var values = [
+      [uuidv1()], [uuidv1()],
+    ];
+    var queryText = format('INSERT INTO links (id) VALUES %L', values);
+    console.log(queryText);
+    pool.query(queryText, (err, res) => {
+      console.log(err, res);
+      pool.end();
+    });
 }
 
 exports.handler = function (event, context, callback) {
     x();
 }
-
-x();
