26d72d105e24ab559d20de3e1e267b6173c14f13
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-06-03 16:01:41 +0100

    make it work

--- lambdas/gu-bechdel-fronts-analyser/index.js
+++ lambdas/gu-bechdel-fronts-analyser/index.js
@@ -1,9 +1,14 @@
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'eu-west-1'});
+
+ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+
+
 const doc = require('dynamodb-doc');
 const Q = require('kew');
 const bechdelScore = require('gu-bechdel');
 const fetch = require("node-fetch");
 
-const dynamo = new doc.DynamoDB({ region: 'eu-west-1' });
 const namesJsonUrl = 'https://s3-eu-west-1.amazonaws.com/bechdel-test-names/names.json'
 
 const capiKey = process.env.CapiKey;
@@ -11,6 +16,7 @@ const pathsString = process.env.Paths;
 
 const pathsArray = pathsString.split(",");
 var urls = formUrls(pathsArray);
+
 
 function formUrls(paths) {
     return paths.map(x => "http://api.nextgen.guardianapps.co.uk" + x + "/lite.json");
@@ -22,7 +28,7 @@ function putItem(json) {
       TableName: 'bechdel-fronts',
       Item: json
     };
-    dynamo.putItem(
+    ddb.putItem(
         params,
         defer.makeNodeResolver()
     );
@@ -46,46 +52,72 @@ function x() {
         fetchResponses.then(function(responses) {
         Promise.all(responses).then(function(json){
             var objects = json.map((element,index) => {
-                var item = {};
+                var item = [];
                 var date = new Date();
-                item["time"] = date.toString();
-                item["front"] = pathsArray[index];
-                item["links"] = [];
+                var promises = [];
+                element.collections.map((collection, containerIndex) => {
+                  if(collection.content){
+                      collection.content.map((content, contentIndex) => {
 
-                if(element.collections){
-                    element.collections.map((collection, containerIndex) => {
-                        if(collection.content){
-                            collection.content.map((content, contentIndex) => {
-                              console.log("contentID: " + content.id);
-                              console.log(capiKey);
-                                bechdelScore.getArticleScoreFromPath(content.id, names, capiKey).then(x => {
-                                    var breakdown = x.breakdown;
-                                    var score = x.score;
-                                    var linkData = {
-                                        link : content.id,
-                                        containerIndex : containerIndex,
-                                        containerName : collection.displayName,
-                                        contentIndex : contentIndex,
-                                        breakdown: breakdown,
-                                        score: score
-                                    };
-                                    item["links"].push(linkData);
-                                });
-                            });
-                        }
-                    });
 
-                }
-                console.log("item: " + item["front"]);
-                putItem(item)
-                .then(data => {
-                  console.log("success");
-                  response(data, 200)
-                })
-                .fail(err => {
-                  console.log(err);
-                  response(err, 500)
+                        promises.push(bechdelScore.getArticleScoreFromPath(content.id, names, capiKey).then(x => {
+                              var breakdown = x.breakdown;
+                              var score = x.score;
+                              var linkData = {
+                                  time: {
+                                    S: date.toString()
+                                  },
+                                  front: {
+                                    S: pathsArray[index]
+                                  },
+                                  link : {
+                                    S: content.id
+                                  },
+                                  containerIndex : {
+                                    N: containerIndex.toString()
+                                  },
+                                  containerName : {
+                                    S: collection.displayName,
+                                  },
+                                  contentIndex : {
+                                    N: contentIndex.toString()
+                                  },
+                                  breakdown: {
+                                    S: breakdown.toString()
+                                  },
+                                  score: {
+                                    N: score.toString()
+                                  }
+                              };
+                              item.push(linkData);
+                              return true;
+                              // putItem(linkData)
+                              // .then(data => {
+                              //   console.log("success");
+                              //   response(data, 200)
+                              // })
+                              // .fail(err => {
+                              //   console.log(err);
+                              //   response(err, 500)
+                              // });
+                          }));
+
+                      });
+                  }
                 });
+                Promise.all(promises).then(x => {
+                  putItem(item[0])
+                  .then(data => {
+                    console.log("success");
+                    response(data, 200)
+                  })
+                  .fail(err => {
+                    console.log(err);
+                    response(err, 500)
+                  });
+                });
+
+
             });
             });
         });
